7f54cd6ae21f0de32a89129b3c100ff2cb1b1a4c
Fernando G. Ponce <data.fernan@gmail.com>
2026-03-02 21:55:54 -0500

    estudiante 1:1

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -2,7 +2,14 @@ import {View, Text, StyleSheet, Button} from 'react-native'
 
 export const Product = ({navigation}) =>{
     return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
+        <Text>Bienvenidos a mi tienda, soy fer</Text>
+        <View>
+
+            <Text style={styles.producto1} >azucar 0.50 </Text>
+            <Text style={styles.producto2}>huevos 0.17</Text>
+            <Text style={styles.producto3}>platanos 0.15</Text>
+        </View>
+
         <Button
             title='HOME'
             onPress={()=>{
@@ -16,8 +23,17 @@ export const Product = ({navigation}) =>{
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#68d13f',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    producto1:{
+        color:"#8945b6"
+    },
+    producto2:{
+        color:"#6e8716"
+    },
+    producto3:{
+        color:"#c0b630"
     },
   });
