14196feddb2ba56063d57c1c902f52adeb9825d6
KarenAvila29 <avilamore20@gmail.com>
2026-03-02 22:00:27 -0500

    Modificar app

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -13,7 +13,7 @@ export const Product = ({navigation}) =>{
         <Button
             title='HOME'
             onPress={()=>{
-                navigation.navigate('HomeNav');
+                navigation.navigate('WellcomeNav');
             }}
         />
     </View>
